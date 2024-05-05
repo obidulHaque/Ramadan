a8393dfca943220ac9a9e08b78395816ab2eaac2
obidulHaque <obaidulhoqejoy@gmail.com>
2024-05-06 01:40:43 +0600

    first commit

---
+++ Ramadan.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded',function(){
+    setTimeout(() => {
+        document.getElementById('Salat').innerText="Ramadan Karim ";
+        document.getElementById("Salat").style.opacity =1;
+    }, 8000); // 8 seconds
+});
